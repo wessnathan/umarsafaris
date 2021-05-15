3dd386ba1d14ebb3c7a6cb497ecdba45ed272a4a
wessnathan <amokenathaniel@gmail.com>
2021-05-15 15:57:39 +0300

    addesd heroku to allowed hosts

--- umarsafaris/settings.py
+++ umarsafaris/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fpmdmr6bzr8(-kqj^a7351az@&7!p&ipq@iqr2hp0sd@oxr71i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.147', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.43.147', '127.0.0.1', 'umarsafaris.herokuapp.com']
 
 
 # Application definition
