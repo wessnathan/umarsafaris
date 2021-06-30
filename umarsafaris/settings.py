8a025d551ceb429996d05ceb81c02745a7b7465a
wessnathan <amokenathaniel@gmail.com>
2021-06-30 21:37:18 +0300

    Edited admin panel

--- umarsafaris/settings.py
+++ umarsafaris/settings.py
@@ -177,7 +177,7 @@ AWS_STORAGE_BUCKET_NAME = 'umarsafaris'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- umarsafaris/urls.py
+++ umarsafaris/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
 urlpatterns += [ path(r'^tz_detect/', include('tz_detect.urls')),]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
