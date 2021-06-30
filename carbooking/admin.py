8ab533ead357669aea9b0af0598ec92985956dcf
wessnathan <amokenathaniel@gmail.com>
2021-06-30 21:28:46 +0300

    Editef admin panel

--- carbooking/admin.py
+++ carbooking/admin.py
@@ -4,14 +4,12 @@ from carbooking.models import Cars_Available, Current_caruser_booking
 # Register your models here.
 class Cars_AvailableAdmin(admin.ModelAdmin):
     list_display = ('name', 'model', 'milage', 'image')
-    list_filter = ('model', 'publish')
     search_fields = ('name', 'model')
     prepopulated_fields = {"slug": ("name",)}
 
 
 class Current_caruser_bookingAdmin(admin.ModelAdmin):
     list_display = ('First_Name', 'Second_Name', 'ID_No', 'carpicked', 'Email', 'Contact' )
-    list_filter = ('ID_No', 'carpicked', 'Email',)
     search_fields = ('Email', 'Contact', 'Second_Name', 'ID_No',)
 
 
--- safarisapp/admin.py
+++ safarisapp/admin.py
@@ -6,19 +6,16 @@ from .models import *
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('fname', 'title', 'tel', 'email', 'date')
-    list_filter = ('fname', 'email')
     search_fields = ('fname', 'tel', 'email', 'date')
 
 
 class SendMessageAdmin(admin.ModelAdmin):
     list_display = ('email', 'dt')
-    list_filter = ('email')
     search_fields = ('email', 'dt')
 
 
 class OurteamAdmin(admin.ModelAdmin):
     list_display = ('name', 'position')
-    list_filter = ('position')
     search_fields = ('position', 'email')
     
     
